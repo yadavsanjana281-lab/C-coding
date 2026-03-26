c5760b27d8b971f7047224984cd5f7b0ac8f0ba0
yadavsanjana281-lab <yadavsanjana281@gmail.com>
2026-03-26 15:49:50 +0530

    Update palindrome_array.c

--- palindrome_array.c
+++ palindrome_array.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 
-int main() 
+void main() 
 {
-   int num,i=0;
-   printf("Enter a number");
+   int num,i=0,a[10];
+   printf("Enter a number:");
    scanf("%d",&num);
-   int a[10]={0};
    while(num!=0)
    {
        a[i]=num%10;
        num=num/10;
        i++;
    }
-   for(int j=0;j<i/2;j++)
+   for(int j=0;j<i;j++)
    {
        if(a[j]!=a[i-j-1])
+       {
        printf("its not a palindrome");
-       else
-       printf("is a palindrome");
+       }
    }
-   
-
-    return 0;
+   printf("is a palindrome");
 }
